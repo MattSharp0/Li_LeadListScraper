c2997f01dea892875b7ac9c86261b18a6438dcee
Matt Sharp <matthew.sharp3@gmail.com>
2021-07-02 14:09:01 -0600

    Added fuction desciptions, exception handling

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from openpyxl import Workbook
-from config import username, password
+from config import USERNAME, PASSWORD
 
 lead_list_name = input('Type lead list name: ')
 
 
 def scrape_lead_list(lead_list_name):
-    # set options
+    '''
+    Scrapes LinkedIn profile links from provided sales navigator lead list and returns them as a list.
+
+    Requires String Arg: lead_list_name
+
+    Requires sales navigator credentials imported as USERNAME and PASSWORD
+    '''
+
+    # set webdriver options
     options = Options()
     options.add_argument('--headless')
     options.add_argument('window-size=1920x1080')
@@ -21,47 +29,56 @@ def scrape_lead_list(lead_list_name):
 
         # sign in
         username_box = browser.find_element_by_id('username')
-        username_box.send_keys(username)
+        username_box.send_keys(USERNAME)
         password_box = browser.find_element_by_id('password')
-        password_box.send_keys(password)
-        try:
-            signin_button = browser.find_element_by_xpath(
-                '//*[@id="app__container"]/main/div[2]/form/div[3]/button')
-            signin_button.click()
-            print('\nSigned in, loading hompage...')
-        except NoSuchElementException():
-            exit(1)
+        password_box.send_keys(PASSWORD)
+
+        signin_button = browser.find_element_by_xpath(
+            '//*[@id="app__container"]/main/div[2]/form/div[3]/button')
+        signin_button.click()
+        print('\nSigned in, loading hompage...')
 
         # Wait for homepage to load, then go to Lead lists
-        WebDriverWait(browser, 10).until(
-            lambda b: b.find_element_by_id('ember14'))
         try:
-            lead_lists = browser.find_element_by_id('ember14')
-            lead_lists.click()
-            print('\nLoading lead lists page...')
-        except NoSuchElementException():
+            WebDriverWait(browser, 10).until(
+                lambda b: b.find_element_by_id('ember14'))
+        except TimeoutException():
+            print(
+                '\nTimed out loading homepage; could not find "Lead Lists" element (id=ember14)')
             exit(1)
+
+        lead_lists = browser.find_element_by_id('ember14')
+        lead_lists.click()
+        print('\nLoading lead lists page...')
 
         # Wait for Lead lists to load, then go to specified list
-        WebDriverWait(browser, 10).until(lambda b: b.find_element_by_xpath(
-            f"//*[text()='{lead_list_name}']//ancestor::a"))
         try:
-            lead_list = browser.find_element_by_xpath(
-                f"//*[text()='{lead_list_name}']//ancestor::a")
-            lead_list.click()
-            print('\nLoading target lead list...')
+            WebDriverWait(browser, 10).until(lambda b: b.find_element_by_xpath(
+                f"//*[text()='{lead_list_name}']//ancestor::a"))
+        except TimeoutException():
+            print(f'\nTimed out loading "{lead_list_name}" element')
+            exit(1)
         except NoSuchElementException():
+            print(
+                f'\nCould not find "{lead_list_name}" on page')
             exit(1)
 
+        lead_list = browser.find_element_by_xpath(
+            f"//*[text()='{lead_list_name}']//ancestor::a")
+        lead_list.click()
+        print('\nLoading target lead list...')
+
         # wait for target Lead List to load, then add <a> elements to list
-        WebDriverWait(browser, 10).until(lambda b: b.find_elements_by_class_name(
-            'lists-detail__view-profile-name-link'))
         try:
-            leads = browser.find_elements_by_class_name(
-                'lists-detail__view-profile-name-link')
-            print(f'\nSaving {len(leads)} lead elements to list...')
-        except NoSuchElementException():
+            WebDriverWait(browser, 10).until(lambda b: b.find_elements_by_class_name(
+                'lists-detail__view-profile-name-link'))
+        except TimeoutException():
+            print('\nTimed out loading lead list')
             exit(1)
+
+        leads = browser.find_elements_by_class_name(
+            'lists-detail__view-profile-name-link')
+        print(f'\nSaving {len(leads)} lead elements to list...')
 
         # Get url (href) from a tag and add to list
         list_of_lead_links = [lead.get_attribute('href') for lead in leads]
@@ -76,22 +93,38 @@ def scrape_lead_list(lead_list_name):
 
 
 def write_to_excel(link_list, lead_list_name):
+    '''
+    Takes a list of links and writes them to an excel document with the list name as the title
+
+    Requires list of strings/ints
+
+    Requires list name
+    '''
+
+    # Quit if list has no items
     if len(link_list) < 1:
         print('\nList empty!')
         exit(1)
 
+    # Create document and sheet
     print('\nWriting list to excel...')
-    title = ((lead_list_name.split()[0]) + ' leads')
     wb = Workbook()
     sheet = wb.create_sheet('lead list', 0)
+
+    # Format title
+    title = ((lead_list_name.split()[0]) + ' leads')
+
+    # Write title
     sheet['A1'].value = title
 
+    # Write links to A column
     r = 2
     for link in link_list:
         box = sheet.cell(r, 1)
         box.value = link
         r += 1
 
+    # Save document with title
     wb.save(f'{title}.xlsx')
 
     print(f'\nSaved {len(link_list)} lead links to {title}.xlsx')
